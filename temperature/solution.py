03c200da69fb8ca09bbb2a312bf80a0e4467a79d
Olivier Leroy <leroy.oli@gmail.com>
2022-11-21 08:58:53 -0500

    wrong prec

--- temperature/solution.py
+++ temperature/solution.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+
+[x, y] = [int(i) for i in input().split()]
+
+res = x / (1 - y)
+
+if x == 0 & y == 1:
+    print("ALL GOOD")
+elif y == 1:
+    print("IMPOSSIBLE")
+else:
+    if float.is_integer(res):
+        print(int(res))
+    else:
+        print("%.9f"%res)
